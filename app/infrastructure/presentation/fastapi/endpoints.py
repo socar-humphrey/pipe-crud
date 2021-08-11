eb30ef50044d16d9fa4f86746c950f89ff1dfefd
socar-humphrey <humphrey@socar.kr>
2021-08-11 22:28:48 +0900

    feat(infrastructure): endpoints를 추가한다

---
+++ app/infrastructure/presentation/fastapi/endpoints.py
@@ -0,0 +1,41 @@
+from fastapi import APIRouter, Depends
+
+from app.application.uow import UnitOfWork
+from app.application.usecases import get_user_by_id
+
+api_router = APIRouter(prefix="/api/v1")
+
+
+@api_router.get("/users/{user_id}")
+def get_user(user_id: str, uow: UnitOfWork = Depends()):
+    return get_user_by_id(user_id=user_id, uow=uow)
+
+
+@api_router.post("/users")
+def add_user():
+    pass
+
+
+@api_router.delete("/users/{user_id}")
+def delete_user(user_id: str):
+    pass
+
+
+@api_router.get("/posts/{post_id}")
+def get_posts_by_id(post_id: str):
+    pass
+
+
+@api_router.post("/posts")
+def create_post(post_id: str):
+    pass
+
+
+@api_router.get("/posts")
+def get_posts_by_user_id(user_id: str):
+    pass
+
+
+@api_router.delete("/posts/{post_id}")
+def delete_post(post_id: str):
+    pass
